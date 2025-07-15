64a5842891dfa9c798d42a85741efe70d5e01d1a
Mahnoor Khalid <mahnoor@redapes.se>
2025-07-15 21:27:11 +0500

    add mongosose pkg

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ import profitRoutes from "./routes/profits.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
 connectDB();
-app.use(express.json());
+
 const app = express();
+app.use(express.json());
      app.use(
        cors()
      );
