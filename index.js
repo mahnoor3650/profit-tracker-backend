b41fd6dcae2ec2eb47be4cf1c1694c68ebb95843
Mahnoor Khalid <mahnoor@redapes.se>
2025-07-15 21:07:36 +0500

    fix vercel issue

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ const app = express();
      app.use(
        cors()
      );
-app.use(express.json());
 
+app.use(express.json());
 app.use("/api/investment", investmentRoutes);
 app.use("/api/profits", profitRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
